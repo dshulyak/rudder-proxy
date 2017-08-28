51485660a348be4c8c361a97bb282ef6f70ee277
Dmitry Shulyak <yashulyak@gmail.com>
2017-08-28 11:02:11 +0300

    build.sh replicates makefile functionality

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-glide install --strip-vendor
-pushd vendor/istio.io/api/
-protoc --go_out=plugins=grpc:. ./proxy/v1/config/*.proto
-popd
-go build -o istio-proxy ./cmd/
-docker build -t yashulyak/istio-rudder-proxy .
-docker push yashulyak/istio-rudder-proxy
